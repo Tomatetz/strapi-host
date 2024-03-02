80d83232df69bd0a03c848accd8641cc8fc713c3
Aleksei Ganikhin <tomatetz@gmail.com>
2024-03-02 10:52:37 +0100

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,17 @@
 module.exports = () => ({
   upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_SECRET,
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
   },
 });
